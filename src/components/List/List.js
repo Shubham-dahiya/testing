796055b44d7f9f018073726d83bef6bb7122bd2b
shubham <dahiya.shubham1234.com>
2022-05-23 14:13:17 +0530

    commit

---
+++ src/components/List/List.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const List = () =>{
+    return (
+        <h1>list</h1>
+    );
+}
+export default List;
---
+++ src/components/PlaceDetails/PlaceDetails.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const PlaceDetails = () =>{
+    return (
+        <h1>placedetails</h1>
+    );
+}
+export default PlaceDetails;
